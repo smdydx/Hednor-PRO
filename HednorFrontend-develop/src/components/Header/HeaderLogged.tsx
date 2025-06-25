9651ba91f7395f3ecc08fd48c2f12ef1b32dc415
Samad Alam <77625439+smdydx@users.noreply.github.com>
2025-06-25 19:55:26 +0000

    Restored to '0d9010c517436a278e4664992e277a202b520d28'
    Replit-Restored-To: 0d9010c517436a278e4664992e277a202b520d28

--- HednorFrontend-develop/src/components/Header/HeaderLogged.tsx
+++ HednorFrontend-develop/src/components/Header/HeaderLogged.tsx
@@ -12,19 +12,3 @@ const HeaderLogged: FC<HeaderLoggedProps> = () => {
 };
 
 export default HeaderLogged;
-import React, { FC } from "react";
-import MainNav2Logged from "./MainNav2Logged";
-import CartDropdown from "./CartDropdown";
-import AvatarDropdown from "./AvatarDropdown";
-
-export interface HeaderLoggedProps {}
-
-const HeaderLogged: FC<HeaderLoggedProps> = () => {
-  return (
-    <div className="nc-HeaderLogged sticky top-0 w-full z-40 ">
-      <MainNav2Logged />
-    </div>
-  );
-};
-
-export default HeaderLogged;
